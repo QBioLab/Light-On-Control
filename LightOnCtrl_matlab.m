56e377cded2af3624effec560d0ff2a8a45907ec
HF <moyuejian@outlook.com>
2019-07-26 15:18:04 +0800

    update matlab control code

--- LightOnCtrl_matlab.m
+++ LightOnCtrl_matlab.m
@@ -1,10 +1,13 @@
 % Initalize Hardware
+pkg load instrument-control
 ser = serial('COM1', 57600);
 fopen(ser);
 
 % Send Command
-fprintf(ser,'&ALL_0100_S#'); % All open
-fprintf(ser,'&ALL_0000_S#'); % All close
+%fprintf(ser,'&ALL_0100_S#'); % All open
+%fprintf(ser,'&ALL_0000_S#'); % All close
+srl_write(ser,'&ALL_0000_S#'); %ALL close
+ 
 
 % Close serial port
 %fclose(ser);
